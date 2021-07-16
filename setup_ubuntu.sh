9af24fea6044b07322a995fbedad396db6e7847f
Hamish Ivison <hamishivi@gmail.com>
2021-07-16 10:41:19 +1000

    fix

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -7,7 +7,6 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt -y install nodejs vim
 # git stuff
 git clone https://github.com/smogon/pokemon-showdown.git
-git clone https://hamishivi:0baa31c91fc58c61cb8d0163d9d252ddfe09f4c3@github.com/hamishivi/stunfisk-rl.git
 
 # build showdown
 cd pokemon-showdown
